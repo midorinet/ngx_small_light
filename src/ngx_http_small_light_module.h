229c510a3ca5061e13e77f5493825c3e49385ca5
midorinet <glen@lumanau.web.id>
2020-07-28 18:35:47 +0700

    Update ngx_http_small_light_module.h

--- src/ngx_http_small_light_module.h
+++ src/ngx_http_small_light_module.h
@@ -95,7 +95,9 @@ typedef struct {
     ngx_http_small_light_color_t bc;
     ngx_int_t                    ix;
     ngx_int_t                    iy;
+    ngx_int_t                    img_dpr;
     double                       aspect;
+    double                       fitfactor;
     ngx_int_t                    pt_flg;
     ngx_int_t                    scale_flg;
     ngx_int_t                    jpeghint_flg;
